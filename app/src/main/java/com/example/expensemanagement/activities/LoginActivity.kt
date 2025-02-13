d1873eb3c4bc3e2b92e0557a63e170ed1a36190d
DinhNgocThe <dinhngocthe.work@gmail.com>
2025-02-13 14:34:26 +0700

    Add initEvents in LanguageBottomSheetFragment

--- app/src/main/java/com/example/expensemanagement/activities/LoginActivity.kt
+++ app/src/main/java/com/example/expensemanagement/activities/LoginActivity.kt
@@ -1,28 +1,35 @@
 package com.example.expensemanagement.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.expensemanagement.R
+import com.example.expensemanagement.bottomsheets.LanguageBottomSheetFragment
 import com.example.expensemanagement.databinding.ActivityLoginBinding
-import com.example.expensemanagement.databinding.ActivityMainBinding
 
+@SuppressLint("StaticFieldLeak")
+private lateinit var loginBinding: ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(loginBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         initEvents()
     }
 
     private fun initEvents() {
+        loginBinding.btnChangeLanguageLoginActivity.setOnClickListener{
+            val bottomSheet = LanguageBottomSheetFragment()
+            bottomSheet.show(supportFragmentManager, "LanguageBottomSheet")
+        }
     }
 }
